9055fe15c78f5285dde7f4d82a3ef40aee3e6512
Yasin <yasinerdemli35@icloud.com>
2022-04-23 15:30:51 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Yasin Erdemli on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
